1419056ef44fc234a0f11c0d288e4221595a7717
Curtis Netterville <curtis@netterville.com>
2026-08-07 08:44:48 -0500

    Initial Commit

---
+++ Weather Widget/Weather_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_WidgetApp.swift
+//  Weather Widget
+//
+//  Created by Curtis Netterville on 8/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Weather_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weather WidgetTests/Weather_WidgetTests.swift
@@ -0,0 +1,19 @@
+//
+//  Weather_WidgetTests.swift
+//  Weather WidgetTests
+//
+//  Created by Curtis Netterville on 8/7/26.
+//
+
+import Testing
+@testable import Weather_Widget
+
+struct Weather_WidgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
